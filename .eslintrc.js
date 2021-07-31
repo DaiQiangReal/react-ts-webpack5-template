0a5a283bd419e40d59b0dcf8fac5f6f35dd0922c
DaiQiangReal <daiqianghaha@foxmail.com>
2021-08-01 02:05:02 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2021,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/display-name": 0,
+        indent: ["error", 4],
+        "no-unused-vars":1,
+        "@typescript-eslint/ban-ts-comment":0,
+        "brace-style": [2, "1tbs"],
+        "rest-spread-spacing": ["error"],
+        "no-cond-assign": 2,
+        "no-const-assign": 2,
+        "no-dupe-keys": 2,
+        "no-empty": 2,
+        "no-eq-null": 2,
+        "no-mixed-spaces-and-tabs": [2, false],
+        "guard-for-in": 1,
+        "prefer-const": 1,
+        "prefer-spread": 1,
+        "quotes": [2, "double", { "avoidEscape": true }],
+        "curly":1,
+        "comma-dangle":2,
+        "comma-style":2,
+        "dot-location":2,
+        "eol-last":2,
+        "new-cap":2,
+        "no-duplicate-imports":2,
+        "no-multi-spaces":2,
+        "use-isnan":2,
+        "space-infix-ops": ["error", { "int32Hint": false }],
+        "@typescript-eslint/no-explicit-any":0,
+        "comma-spacing":2
+    }
+
+};
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isDev = process.env.DEV === "true";
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+module.exports = {
+    entry: "./src/index",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 8080,
+        hot: true,
+        historyApiFallback: true,
+        host: "0.0.0.0",
+        disableHostCheck: true
+    },
+    output: {
+        publicPath:''
+    },
+    devtool: "source-map",
+    mode: isDev ? "development" : "production",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /bootstrap\.tsx$/,
+            //     loader: "bundle-loader",
+            //     options: {
+            //         lazy: true
+            //     }
+            // },
+            {
+                test: /\.tsx?$/,
+                loader: "esbuild-loader",
+                exclude: /node_modules/,
+                options: {
+                    loader: "tsx",
+                    target: "esnext"
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                loader: "esbuild-loader",
+                exclude: /node_modules/,
+                options: {
+                    loader: "jsx",
+                    target: "esnext"
+                }
+            },
+            {
+                test: /\.s?[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-modules-typescript-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                auto: true,
+                                localIdentName: isDev ? "[path][name]__[local]--[hash:base64:5]" : "[hash:base64:5]",
+                                localIdentContext: path.resolve(__dirname, "src")
+                            }
+
+                        }
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ProgressBarPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+
+        }),
+        new MiniCssExtractPlugin()
+    ]
+};
